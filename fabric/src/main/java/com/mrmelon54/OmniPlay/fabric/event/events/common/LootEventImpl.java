fd3f427d3d76354210fcc1db61ca25cc9a1ecb3c
MrMelon54 <github@mrmelon54.com>
2024-03-31 02:39:52 +0100

    Use remapped architectury in all subprojects

--- fabric/src/main/java/com/mrmelon54/OmniPlay/fabric/event/events/common/LootEventImpl.java
+++ fabric/src/main/java/com/mrmelon54/OmniPlay/fabric/event/events/common/LootEventImpl.java
@@ -2,9 +2,10 @@ package com.mrmelon54.OmniPlay.fabric.event.events.common;
 
 import com.mrmelon54.OmniPlay.event.EventWrapper;
 import com.mrmelon54.OmniPlay.event.events.common.LootEvent;
+import remapped.architectury.event.Event;
 
 public class LootEventImpl {
-//    static Event<LootEvent.ModifyLootTable> resolveModifyLootTable() {
-//        return EventWrapper.of(LootEvent.Inner.MODIFY_LOOT_TABLE, LootEvent::mapModifyLootTable);
-//    }
+    static Event<LootEvent.ModifyLootTable> resolveModifyLootTable() {
+        return EventWrapper.of(LootEvent.Inner.MODIFY_LOOT_TABLE, LootEvent::mapModifyLootTable);
+    }
 }
