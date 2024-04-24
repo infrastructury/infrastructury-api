6cbc0443a5f41cbf820062b19105a03cfa774e82
MrMelon54 <github@mrmelon54.com>
2024-04-24 23:05:33 +0100

    Fix registry supplier for 1.20.1

--- common/src/main/java/com/mrmelon54/infrastructury/registry/registries/RegistrySupplier.java
+++ common/src/main/java/com/mrmelon54/infrastructury/registry/registries/RegistrySupplier.java
@@ -7,8 +7,6 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import remapped.architectury.impl.RegistrySupplierImpl;
 import remapped.architectury.registry.registries.Registrar;
 import remapped.architectury.registry.registries.RegistrarManager;
 
@@ -19,7 +17,7 @@ import java.util.stream.Stream;
 
 public interface RegistrySupplier<T> extends DeferredSupplier<T>, Holder<T> {
     static <T> RegistrySupplier<T> of(remapped.architectury.registry.registries.RegistrySupplier<T> supplier) {
-        return new RegistrySupplier<T>() {
+        return new RegistrySupplier<>() {
             @Override
             public remapped.architectury.registry.registries.RegistrySupplier<T> architectury$convert() {
                 return supplier;
@@ -57,57 +55,101 @@ public interface RegistrySupplier<T> extends DeferredSupplier<T>, Holder<T> {
 
             @Override
             public T value() {
+                #if MC_VER > MC_1_20_2
                 return supplier.value();
+                #else
+                return supplier.get();
+                #endif
             }
 
             @Override
             public boolean isBound() {
+                #if MC_VER > MC_1_20_2
                 return supplier.isBound();
+                #else
+                return false;
+                #endif
             }
 
             @Override
             public boolean is(ResourceLocation location) {
+                #if MC_VER > MC_1_20_2
                 return supplier.is(location);
+                #else
+                return location.equals(getId());
+                #endif
             }
 
             @Override
             public boolean is(ResourceKey<T> resourceKey) {
+                #if MC_VER > MC_1_20_2
                 return supplier.is(resourceKey);
+                #else
+                return supplier.isPresent();
+                #endif
             }
 
             @Override
             public boolean is(Predicate<ResourceKey<T>> predicate) {
+                #if MC_VER > MC_1_20_2
                 return supplier.is(predicate);
+                #else
+                return predicate.test(getKey());
+                #endif
             }
 
             @Override
             public boolean is(TagKey<T> tagKey) {
+                #if MC_VER > MC_1_20_2
                 return supplier.is(tagKey);
+                #else
+                return tagKey.location().equals(getId());
+                #endif
             }
 
             @Override
             public Stream<TagKey<T>> tags() {
+                #if MC_VER > MC_1_20_2
                 return supplier.tags();
+                #else
+                return Stream.empty();
+                #endif
             }
 
             @Override
             public @NotNull Either<ResourceKey<T>, T> unwrap() {
+                #if MC_VER > MC_1_20_2
                 return supplier.unwrap();
+                #else
+                return !supplier.isPresent() ? Either.left(supplier.getKey()) : Either.right(supplier.get());
+                #endif
             }
 
             @Override
             public @NotNull Optional<ResourceKey<T>> unwrapKey() {
+                #if MC_VER > MC_1_20_2
                 return supplier.unwrapKey();
+                #else
+                return Optional.of(supplier.getKey());
+                #endif
             }
 
             @Override
             public @NotNull Kind kind() {
+                #if MC_VER > MC_1_20_2
                 return supplier.kind();
+                #else
+                return !supplier.isPresent() ? Kind.REFERENCE : Kind.DIRECT;
+                #endif
             }
 
             @Override
             public boolean canSerializeIn(HolderOwner<T> owner) {
+                #if MC_VER > MC_1_20_2
                 return supplier.canSerializeIn(owner);
+                #else
+                return false;
+                #endif
             }
         };
     }
